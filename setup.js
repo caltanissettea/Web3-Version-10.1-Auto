40916592b775c667b0246ffa4f116fb8950be822
caltanissettea <129787081+caltanissettea@users.noreply.github.com>
2024-04-16 12:28:34 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,6 +1,5 @@
 require('@babel/register');
 require('ts-node').register({ transpileOnly: false });
-
 require('./helpers/setup-helper');
 
 window.SVGPathElement = window.SVGPathElement || { prototype: {} };
